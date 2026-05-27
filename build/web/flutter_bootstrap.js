446fc8fd1fb37e09b3ec500f4521fe31d631e7a1
Maycol Melgarejo <mfmelgarejo04@gmail.com>
2026-05-27 14:51:18 -0500

    actualización de archivo web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3667192418" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1531343870" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
